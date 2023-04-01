95755735b34cb090cf34fccf67f46adc12eb3b01
Alexandr Rodionov <alexrodionby@gmail.com>
2023-04-01 15:25:58 +0300

    added an empty starter project

---
+++ FilmFusion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmFusion
+//
+//  Created by Alexandr Rodionov on 1.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
