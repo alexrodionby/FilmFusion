13cfca7ca868f235191cdb6f86de5f5d1d4c18fd
Alexandr Rodionov <alexrodionby@gmail.com>
2023-04-03 00:23:11 +0300

    added a new extension

---
+++ FilmFusion/Helpers/Extensions/UIImage + Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIImage + Extensions.swift
+//  FilmFusion
+//
+//  Created by Alexandr Rodionov on 3.04.23.
+//
+
+import UIKit
+
+extension UIImage {
+    func resized(to size: CGSize) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: size)
+        let image = renderer.image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+        return image
+    }
+}
--- FilmFusion/Screens/TabBar/TabBarVC.swift
+++ FilmFusion/Screens/TabBar/TabBarVC.swift
@@ -41,70 +41,14 @@ final class TabBarVC: UITabBarController {
     private func generateVC(viewController: UIViewController, image: UIImage?, selectedImage: UIImage?, tag: Int) -> UINavigationController {
         
         let iconSize = CGSize(width: tabBar.bounds.height * 0.7, height: tabBar.bounds.height * 0.7)
-        viewController.tabBarItem.title = title
         viewController.tabBarItem.image = image?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal).resized(to: iconSize)
         viewController.tabBarItem.selectedImage = selectedImage?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal).resized(to: iconSize)
         viewController.tabBarItem.tag = tag
-        
-        
-//        let iconImage = UIImage(named: "icon-name")?.resized(to: iconSize)
-//        tabBarItem.image = iconImage
-
-        
         return UINavigationController(rootViewController: viewController)
     }
     
     private func setTabBarAppearance() {
         selectedIndex = 2 // Стартуем с экрана номер 3
- 
-//        let width = tabBar.bounds.width
-//        let height = tabBar.bounds.height
-//
-//        tabBar.itemWidth = width / 3
-//        tabBar.itemPositioning = .centered
-
-
-    }
-    
-//    private func configureTabBar() {
-//        tabBar.tintColor = Resources.Colors.tabBarActive
-//        tabBar.barTintColor = Resources.Colors.tabBarInActive
-//        tabBar.backgroundColor = Resources.Colors.tabBarLight // Реализовать при смене темы на другую
-//
-//        let searchController = UIViewController()
-//        let recentController = UIViewController()
-//        let homeController = UIViewController()
-//        let favoritesController = UIViewController()
-//        let settingsController = UIViewController()
-//
-//        let searchNavigation = UINavigationController(rootViewController: searchController)
-//        let recentNavigation = UINavigationController(rootViewController: recentController)
-//        let homeNavigation = UINavigationController(rootViewController: homeController)
-//        let favoritesNavigation = UINavigationController(rootViewController: favoritesController)
-//        let settingsNavigation = UINavigationController(rootViewController: settingsController)
-//
-//        searchController.tabBarItem = UITabBarItem(title: "", image: Resources.Images.TabBar.search, tag: Tabs.search.rawValue)
-//        recentController.tabBarItem = UITabBarItem(title: "", image: Resources.Images.TabBar.recentVideo, tag: Tabs.recentVideo.rawValue)
-//        homeController.tabBarItem = UITabBarItem(title: "", image: Resources.Images.TabBar.home, tag: Tabs.home.rawValue)
-//        favoritesController.tabBarItem = UITabBarItem(title: "", image: Resources.Images.TabBar.favorites, tag: Tabs.favorites.rawValue)
-//        settingsNavigation.tabBarItem = UITabBarItem(title: "", image: Resources.Images.TabBar.settings, tag: Tabs.settings.rawValue)
-//
-//        setViewControllers([searchNavigation, recentNavigation, homeNavigation, favoritesNavigation, settingsNavigation], animated: false)
-//
-//    }
-    
-
-
-}
-
-extension UIImage {
-    
-    func resized(to size: CGSize) -> UIImage {
-        let renderer = UIGraphicsImageRenderer(size: size)
-        let image = renderer.image { _ in
-            draw(in: CGRect(origin: .zero, size: size))
-        }
-        return image
     }
     
 }
