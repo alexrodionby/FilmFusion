72cd7727c022a249de0f45fa8b319a71f4c6dc54
Sergey Azimov <74828253+FarbosZ@users.noreply.github.com>
2023-04-13 20:32:08 +0300

    add method show stars

--- FilmFusion/Screens/Detail/Controller/DetailVC.swift
+++ FilmFusion/Screens/Detail/Controller/DetailVC.swift
@@ -53,9 +53,9 @@ class DetailVC: UIViewController {
         return label
     }()
     
+    
     private let rateStarsView:UIStackView = {
         let stackView = UIStackView()
-        
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
         stackView.spacing = 5
@@ -84,17 +84,39 @@ class DetailVC: UIViewController {
     }()
     
     func addStarsToRateView() {
+      
         let numberOfStars = 5
         for _ in 1...numberOfStars {
             let starImage:UIImageView = {
-                let imageView = UIImageView()
+               let imageView = UIImageView()
+
                 imageView.image = UIImage(named: "star-fill")
                 imageView.tintColor = .yellow
-                return imageView
+               return imageView
             }()
             rateStarsView.addArrangedSubview(starImage)
         }
     }
+    //counting and showing average vote
+    func fillStarStackViewWithRating(averageRating: Double, starStackView: UIStackView) {
+        let ratingOutOfFive = averageRating / 2.0
+        let fullStarCount = Int(ratingOutOfFive)
+        let halfStarCount = Int((ratingOutOfFive - Double(fullStarCount)) * 2)
+        let emptyStarCount = 5 - fullStarCount - halfStarCount
+        
+        for (index, subview) in starStackView.arrangedSubviews.enumerated() {
+            if let imageView = subview as? UIImageView {
+                if index < fullStarCount {
+                    imageView.image = UIImage(named: "star-fill")
+                } else if index < fullStarCount + halfStarCount {
+                    imageView.image = UIImage(named: "star-half")
+                } else {
+                    imageView.image = UIImage(named: "star-empty")
+                }
+            }
+        }
+    }
+
     
     var isSaved: Bool = false {
         didSet {
@@ -111,6 +133,7 @@ class DetailVC: UIViewController {
         posterTitle.text = model.titleName
         additionalInfo.dataReleaseLabel.text = model.releaseDate
         additionalInfo.runTimeLabel.text = "\(model.runtime)"
+        print(model.runtime)
         discrpitionView.storyLineTextView.text = model.overview
         voteAverage = model.voteAverage
         isSaved = RealmDataBase.shared.isItemSaved(withName: posterTitle.text!)
@@ -118,6 +141,7 @@ class DetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("\(voteAverage) оценка и \(voteCount) голосов")
         tabBarController?.tabBar.isHidden = true
         watchButton.addTarget(self, action: #selector(watchButtonTapped), for: .touchUpInside)
         view.backgroundColor = UIColor(named: "customBackground")
@@ -126,6 +150,8 @@ class DetailVC: UIViewController {
         setConstraints()
         setupNavBar()
         addStarsToRateView()
+        fillStarStackViewWithRating(averageRating: voteAverage, starStackView: rateStarsView)
+        isMovieFavorite()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -151,6 +177,13 @@ class DetailVC: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "heart"), style: .done, target: self, action: #selector(tappedHeart))
         navigationController?.navigationBar.tintColor = UIColor(named: "customMiniIcon")
         
+    }
+    
+    func isMovieFavorite() {
+        navigationItem.rightBarButtonItem?.image = RealmDataBase.shared.isItemSaved(withName: posterTitle.text!) ?
+        UIImage(systemName: "heart.fill") : UIImage(systemName: "heart")
+        navigationItem.rightBarButtonItem?.tintColor =  RealmDataBase.shared.isItemSaved(withName: posterTitle.text!) ?
+        UIColor(hexString: "514EB6") : UIColor(named: "customMiniLabel")
     }
     
     @objc  func watchButtonTapped(_ sender: UIButton) {
@@ -237,13 +270,7 @@ extension DetailVC {
             castCollection.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             
             castCollection.heightAnchor.constraint(equalToConstant: 50),
-            
-            
-
-            navigationController!.toolbar.heightAnchor.constraint(equalToConstant: 100),
-            watchButton.topAnchor.constraint(equalTo: navigationController!.toolbar.topAnchor, constant: 5),
-            watchButton.centerXAnchor.constraint(equalTo: navigationController!.toolbar.centerXAnchor),
-            
+    
             watchButton.widthAnchor.constraint(equalToConstant: 180),
             watchButton.heightAnchor.constraint(equalToConstant: 55),
             watchButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
